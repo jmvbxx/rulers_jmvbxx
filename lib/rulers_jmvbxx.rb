081432248c93c52601cb368fbaa1142ff8c7e24c
Jason Bornhoft <jmvbxx@gmail.com>
2021-12-17 11:08:03 -0500

    first commit

---
+++ lib/rulers_jmvbxx.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rulers_jmvbxx/version"
+
+module RulersJmvbxx
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rulers_jmvbxx/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RulersJmvbxx
+  VERSION = "0.1.0"
+end

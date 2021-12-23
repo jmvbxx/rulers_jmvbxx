5eee99224a9d7ce9fefcf1594354ff0993446651
Jason Bornhoft <jmvbxx@gmail.com>
2021-12-22 19:03:29 -0500

    Get it to work (Chapter two)

--- lib/rulers_jmvbxx.rb
+++ lib/rulers_jmvbxx.rb
@@ -2,16 +2,29 @@
 
 require_relative "rulers_jmvbxx/version"
 require "rulers_jmvbxx/array"
+require "rulers_jmvbxx/routing"
 
 module RulersJmvbxx
   class Error < StandardError; end
 
   # This is is main application class (for now)
   class Application
-    def call(_env)
-      `echo debug > debug.txt`
+    def call(env)
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200, { "Content-Type" => "text/html" },
-       ["Hello from Ruby on RulersJmvbxx!"]]
+       [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end

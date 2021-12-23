fcddc5af10b6f40a7362a5794c55ebcb111bbef7
Jason Bornhoft <jmvbxx@gmail.com>
2021-12-22 19:19:05 -0500

    Final changes for the chapter

--- lib/rulers_jmvbxx.rb
+++ lib/rulers_jmvbxx.rb
@@ -10,6 +10,11 @@ module RulersJmvbxx
   # This is is main application class (for now)
   class Application
     def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404,
+                { "Content-Type" => "text/html" }, []]
+      end
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
